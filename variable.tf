deebf339fc5eb905b90ad293eb9e06c330867d8f
Muzammil-2 <muzammil01vivekengg@gmail.com>
2024-02-22 15:22:35 +0530

    update varibale.tf

---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = file("service-account.json")
+}
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 1
+  description = "number of gke nodes"
+}
+
+variable "machine_type" {
+    default = "e2-micro"
+}
